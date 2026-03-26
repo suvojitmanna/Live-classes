ca88621f449b918b827888f1a489174736b5926f
Suvojit Manna <mannasuvojit84@gmail.com>
2026-03-26 23:41:09 +0530

    fix: remove jsx extension from imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { SessionProvider } from "./context/SessionContext.jsx";
+import { AuthProvider } from "./context/AuthContext";
+import { SessionProvider } from "./context/SessionContext";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
